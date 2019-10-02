1487f76b89d37de2e7c7bb83360f3adcc270b813
Плетнев Никита Сергеевич <45274552+NikitaPletnev@users.noreply.github.com>
2019-10-02 21:52:32 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$.get('names.txt', function(data) {    
-    var lines = data.split("\n");
-
-    $.each(lines, function(n, elem) {
-       $('.app').append('<div>' + elem + '</div>');
-    });
-});
-})
